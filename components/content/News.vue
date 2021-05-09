7f9ba9679067b81ef3d976afbc8ffa25e68228c0
robert1003 <b07902047@ntu.edu.tw>
2021-05-10 01:10:40 +0800

    add FAQ

---
+++ components/content/News.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div
+    p 最新消息和 FAQ 都會跟新在這裡喔！
+    h3 最新消息
+    ul
+      li 報名開始摟！請點
+        a(href="#register") 這裡
+    h3 FAQ
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+
+})
+</script>
+
+<style>
+
+</style>
