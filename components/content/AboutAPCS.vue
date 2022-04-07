ae22220faaaf3d4fcb6fdfd784fbd17abea60ba1
baluteshih <brian369999@gmail.com>
2022-04-08 02:56:25 +0800

    fix: many typo and small features

--- components/content/AboutAPCS.vue
+++ components/content/AboutAPCS.vue
@@ -1,31 +1,22 @@
 <template lang="pug">
-  .container
-    nuxt-content(
-      v-if="section"
-      :id="section.id",
-      :document="section",
-      :key="section.id"
-    )
-    .cutter
+  div   
+    h3 何謂 APCS
+    ul
+      li APCS 為 Advanced Placement Computer Science 的英文縮寫，是指「大學程式設計先修檢測」。其檢測模式乃參考美國大學先修課程（Advanced Placement，AP），與各大學合作命題，並確定檢定用題目經過信效度考驗，以確保檢定結果之公信力。在臺灣，APCS 是目前衡量國高中生程式能力最有公信力的一個指標：目前共有 47 所校系，在個人申請中設立 APCS 組，總計開放 118 個名額，提供具有程式能力的學生額外的入學管道。
+    h3 緣起
+    ul
+      li 近年來，隨著人工智慧崛起，資工系竄升成一大熱門科系，大家開始關注要進入資工系所需具備的能力，而 APCS 檢測也因此備受家長關注。補教業者更積極把握機會，使 APCS 的補教課程如雨後春筍般設立。然而，我們發現補習班的課程教材品質參差不齊，且其中一些還有不少進步空間，有些題目的解答甚至是錯誤的。除此之外，我們也發現這些 APCS 的課程大多集中在大臺北地區等都市，收費也不便宜。
+      li 為解決以上問題，我們決定利用我們的專業能力，#[b 在 2022 暑假舉辦一個針對初學者，以 APCS 考試大綱規劃課程內容的線上營隊] 。雖然線上營隊的舉辦較實體多出許多限制，但考量疫情趨勢與我們期望打破地理限制的初衷，我們仍決定線上舉行，期望讓全臺各地想精進程式能力的初學者都能參與。
 </template>
+
 <script>
 import Vue from 'vue';
 
 export default Vue.extend({
-  data() {
-    return {
-      section: undefined,
-    };
-  },
-  async fetch() {
-    this.section = await this.$content("aboutAPCS").fetch();
-    console.log(this.section);
-  },
+
 })
 </script>
 
-<style lang="scss" scoped>
-p {
-  line-height: 1.8;
-}
+<style>
+
 </style>
